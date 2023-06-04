8adaa5d2667e4108a4a802d39301c3f7c9965852
Sergio Nezhigay <abc1971h@gmail.com>
2023-06-04 21:33:11 +0300

    comments remove

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -16,9 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Global styles={GlobalStyles} />
       <App />
     </HashRouter>
-    {/* <BrowserRouter basename="tweets">
-      <Global styles={GlobalStyles} />
-      <App />
-    </BrowserRouter> */}
   </Provider>
 );
